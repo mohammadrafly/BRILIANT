124c4f272e2aafadab2d4d0dcc7ebcf3de75e9da
mohammadrafly <44152125+mohammadrafly@users.noreply.github.com>
2022-08-28 20:55:34 +0700

    initial upload

--- app/Views/dashboard/pages/examp/on_examp.php
+++ app/Views/dashboard/pages/examp/on_examp.php
@@ -3,7 +3,7 @@
 <?= $this->section('content') ?>
 
         <div class="row">
-		  <div class="col-md-12">
+          <div class="col-md-12">
             <div class="card">
               <div class="card-body">
                 <h6 class="card-title">Examps</h6>
@@ -45,8 +45,8 @@
                 </div>
               </div>
             </div>
-		  </div>
-		</div>
+          </div>
+        </div>
 
                 
 <?= $this->endSection() ?>
